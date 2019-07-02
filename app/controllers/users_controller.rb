99f752dab44a8667ef88193a345ced45245f1b0f
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-02 14:18:37 +0200

    Implement basic Log-in view for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
-  def new 
+  def new
+    @user = User.new
   end
 
   def create 
+    user = User.find_or_create_by(user_params)
+    session[:user_id] = user.id
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
