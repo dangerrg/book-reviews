9062ab2d9edb723fc3e4cca92f246fa41a0117ea
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-04 09:41:04 +0200

    Implement most_recent method

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @reviews = Review.most_recent
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :user
+
+  def self.most_recent
+    all.order(created_at: :desc).limit(6)
+  end
 end
