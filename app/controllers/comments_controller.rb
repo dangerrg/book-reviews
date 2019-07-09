4284c3cd2541ce11ff729b84d8babfe91d7da67a
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-10 01:03:33 +0200

    Process comment form
    Implement create action with strong params

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.new comment_params
+    review = Review.find(params[:review_id])
+    comment.review = review
+    comment.save
+
+    redirect_to review_path(review)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
