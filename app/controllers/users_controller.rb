07e76c66eadb5928f51a592ebfc560e4095888ef
dangerrg <d.rodriguez_galvez@hotmail.com>
2019-07-10 02:14:05 +0200

    Redirect to reviews when user logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create 
     user = User.find_or_create_by(user_params)
     session[:user_id] = user.id
+    redirect_to reviews_path
   end
 
   private
